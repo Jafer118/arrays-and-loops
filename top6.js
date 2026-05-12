8ce6c34b65fdd2cd91a5055d68e6950393c57ef0
Jafer118 <Jafer.Asad@student.gildeopleidingen.nl>
2026-05-12 13:12:57 +0200

    Add files via upload

---
+++ top6.js
@@ -0,0 +1,20 @@
+// Opdracht 1
+const top6Songs = [
+    "Bohemian Rhapsody",
+    "Stairway to Heaven",
+    "Imagine",
+    "Hotel California",
+    "Purple Rain",
+    "Smells Like Teen Spirit"
+];
+
+// Opdracht 2
+console.log(top6Songs[0]);
+
+// Opdracht 3
+console.log(top6Songs[2]);
+
+// Opdracht 4
+top6Songs.forEach(function(song) {
+    console.log(song);
+});
